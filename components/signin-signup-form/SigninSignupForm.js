fc128de866b76503f873f49bf44a9262368da3ee
muratdem1r <59487469+muratdem1r@users.noreply.github.com>
2022-09-01 10:22:40 +0300

    user-login forms

--- components/signin-signup-form/SigninSignupForm.js
+++ components/signin-signup-form/SigninSignupForm.js
@@ -1,15 +1,76 @@
-import React from "react";
+import axios from "axios";
+import { useState } from "react";
 import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 import Button from "../ui/Button";
 
 function SigninSignupForm() {
-  const registerSubmitHandler = (e) => {
-    e.preventDefault();
-    toast.success("register");
+  const router = useRouter();
+
+  const [registerInputs, setRegisterInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: "user",
+  });
+  const [loginInputs, setloginInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const registerInputHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setRegisterInputs((prev) => {
+      return { ...prev, [name]: value };
+    });
   };
-  const loginSubmitHandler = (e) => {
+  const loginInputHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setloginInputs((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const registerSubmitHandler = async (e) => {
     e.preventDefault();
-    toast.success("login");
+    try {
+      const res = await axios.post(
+        process.env.NEXT_PUBLIC_HOST + "/api/v1/auth/register",
+        {
+          name: registerInputs.name,
+          email: registerInputs.email,
+          password: registerInputs.password,
+          role: registerInputs.role,
+        }
+      );
+
+      toast.success("You have successfully registered.");
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+      toast.error("This account allready exists!");
+    }
+  };
+  const loginSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        process.env.NEXT_PUBLIC_HOST + "/api/v1/auth/login",
+        {
+          email: loginInputs.email,
+          password: loginInputs.password,
+        }
+      );
+      toast.success("You have successfully logged in.");
+
+      localStorage.setItem("token", res.data.token);
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+      toast.error("Wrong email or password!");
+    }
   };
 
   return (
@@ -20,8 +81,26 @@ function SigninSignupForm() {
       >
         <h1 className="font-bold">Register</h1>
         <div className="flex flex-col gap-2">
+          <label htmlFor="name">Name</label>
+          <input
+            className="border"
+            type="name"
+            name="name"
+            id="registerName"
+            onChange={registerInputHandler}
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-2">
           <label htmlFor="email">Email</label>
-          <input className="border" type="email" name="email" id="email" />
+          <input
+            className="border"
+            type="email"
+            name="email"
+            id="registerEmail"
+            onChange={registerInputHandler}
+            required
+          />
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="password">Password</label>
@@ -29,7 +108,9 @@ function SigninSignupForm() {
             className="border"
             type="password"
             name="password"
-            id="password"
+            id="registerPassword"
+            onChange={registerInputHandler}
+            required
           />
         </div>
         <Button className="bg-slate-400 w-1/3 mx-auto py-2">Submit</Button>
@@ -41,7 +122,14 @@ function SigninSignupForm() {
         <h1 className="font-bold">Login</h1>
         <div className="flex flex-col gap-2">
           <label htmlFor="email">Email</label>
-          <input className="border" type="email" name="email" id="email" />
+          <input
+            className="border"
+            type="email"
+            name="email"
+            id="email"
+            onChange={loginInputHandler}
+            required
+          />
         </div>
         <div className="flex flex-col gap-2">
           <label htmlFor="password">Password</label>
@@ -50,6 +138,8 @@ function SigninSignupForm() {
             type="password"
             name="password"
             id="password"
+            onChange={loginInputHandler}
+            required
           />
         </div>
         <Button className="bg-slate-400 w-1/3 mx-auto py-2">Submit</Button>
