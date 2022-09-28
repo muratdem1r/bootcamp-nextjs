ed7578172ad649c95c83b3d0bf1b2830448732cf
muratdem1r <59487469+muratdem1r@users.noreply.github.com>
2022-09-29 01:33:49 +0300

    baseUrl

--- services/rootApi.js
+++ services/rootApi.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_AWS_HOST + "/api/v1/",
+    baseUrl:
+      "https://vqd86ub631.execute-api.us-east-1.amazonaws.com/Prod/api/v1/",
     prepareHeaders: (headers) => {
       const accessToken = localStorage.getItem("token");
 
