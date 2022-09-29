0227306b355c8b09a0075ff3a3b525a8cac8d50f
muratdem1r <59487469+muratdem1r@users.noreply.github.com>
2022-09-29 11:35:54 +0300

    new api gateway

--- services/rootApi.js
+++ services/rootApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl:
-      "https://vqd86ub631.execute-api.us-east-1.amazonaws.com/Prod/api/v1/",
+      "https://1365backe7.execute-api.us-east-1.amazonaws.com/Prod/api/v1/",
     prepareHeaders: (headers) => {
       const accessToken = localStorage.getItem("token");
 
